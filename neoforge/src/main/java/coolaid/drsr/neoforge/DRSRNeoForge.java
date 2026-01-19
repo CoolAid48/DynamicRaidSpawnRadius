e3d0886cf725f8a8e98f3ea7445307ce49e9ff1c
CoolAid48 <atgunsel@gmail.com>
2026-01-19 01:35:49 -0500

    Fixed NeoForge stuffs
    - Fixed wrong Cloth Config dependency for Neoforge (derp)
    - Got AutoConfig work with NeoForge's build-in mods menu
    - Fixed NeoForge icon.png not loading

--- neoforge/src/main/java/coolaid/drsr/neoforge/DRSRNeoForge.java
+++ neoforge/src/main/java/coolaid/drsr/neoforge/DRSRNeoForge.java
@@ -1,12 +1,25 @@
 package coolaid.drsr.neoforge;
 
 import coolaid.drsr.DynamicRaidSpawnRadius;
+import coolaid.drsr.compat.RaidConfig;
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
+import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 @Mod(DynamicRaidSpawnRadius.MOD_ID)
 public final class DRSRNeoForge {
+
     public DRSRNeoForge() {
         // Run our common setup.
         DynamicRaidSpawnRadius.init();
+
+        // Auto-Config for NeoForge's built-in mods menu
+        ModLoadingContext.get().registerExtensionPoint(
+                IConfigScreenFactory.class,
+                () -> (modContainer, parentScreen) -> AutoConfig.getConfigScreen(RaidConfig.class, parentScreen).get()
+        );
     }
 }
